ae35c80e66a8df2f6809b35e2e5444126924a6e9
Dale <drcorns@hotmail.com>
2016-12-15 21:03:34 -0800

    refactor article data to a model class

--- src/app/article/article.component.ts
+++ src/app/article/article.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Article} from "./article.model";
 
 @Component({
   selector: 'app-article',
@@ -9,23 +10,22 @@ import { Component, OnInit } from '@angular/core';
   }
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  title: string;
-  link: string;
-
+  article: Article;
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
+    this.article = new Article(
+      'Angular 2',
+      'http://angular.io',
+      10
+    );
   }
 
   voteUp() {
-    this.votes ++;
+    this.article.voteUp();
     return false; //prevent propagation refresh
   }
 
   voteDown(){
-    this.votes --;
+    this.article.voteDown();
     return false; //prevent propagation refresh
   }
 
---
+++ src/app/article/article.model.ts
@@ -0,0 +1,26 @@
+/**
+ * article.model
+ * Created by dcorns on 12/15/16
+ * Copyright © 2016 Dale Corns
+ */
+'use strict';
+
+export class Article{
+  title: string;
+  link: string;
+  votes: number;
+  constructor(title: string, link: string, votes: number = 0){
+    this.title = title;
+    this.link = link;
+    this.votes = votes;
+  }
+  voteUp() {
+    this.votes ++;
+    return false; //prevent propagation refresh
+  }
+
+  voteDown(){
+    this.votes --;
+    return false; //prevent propagation refresh
+  }
+}
